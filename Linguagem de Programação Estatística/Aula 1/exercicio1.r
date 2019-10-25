4259f1d96f18a399ddbc3e1d751804e467bb2e77
Charles Cavalcante <e-mail@charles.art.br>
2019-10-24 22:46:21 -0300

    lista 1

---
+++ Linguagem de Programação Estatística/Aula 1/exercicio1.r
@@ -0,0 +1,41 @@
+# Execício 1
+aa <- ((sqrt(16) / 2) * 3 ^ 2) / 2 * (9 - 2 ^ 3)
+aa
+
+# Execício 2
+bb <- -(-2 ^ 3) + ((-1) ^ 0) - sqrt(25 - 3 ^ 2) - (5 ^ 3) / 25
+bb
+
+# Execício 3
+aa > bb
+
+# Execício 4
+a = c(1, 2, 3)
+
+# Execício 5
+a + 1
+
+# Execício 6
+a / 2
+
+# Execício 7
+a * 2
+
+# Execício 8
+b <- c(4, 5, 6)
+
+# Execício 9
+a + b
+
+# Execício 10
+a - b
+
+# Execício 11
+c <- c(1, 3, 5)
+c == a
+
+# Execício 12
+a < c
+
+# Execício 13
+sqrt(a + b)
