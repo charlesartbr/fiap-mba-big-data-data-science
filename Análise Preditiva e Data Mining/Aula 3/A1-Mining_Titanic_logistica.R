28fa809b89f6486e30c16d15a49a24b65c120f84
Charles Cavalcante <e-mail@charles.art.br>
2020-04-05 12:01:00 -0300

    Tabalho final: Aplicação da Árvore de Decisão e Regressão Logística

--- Análise Preditiva e Data Mining/Aula 3/A1-Mining_Titanic_logistica.R
+++ Análise Preditiva e Data Mining/Aula 3/A1-Mining_Titanic_logistica.R
@@ -1,6 +1,6 @@
-# Técnica de Discriminação: regressão logistica.
+# T?cnica de Discrimina??o: regress?o logistica.
 
-# analise exploratória e amostra desenvolvida no scrit anterior (tree)
+# analise explorat?ria e amostra desenvolvida no scrit anterior (tree)
 
 attach(Titanic_train)
 
@@ -9,7 +9,7 @@ logist.model01 <- glm (Survived ~ Pclass+Sex+Age+Embarked+Parch+Fare, family=bin
 
 summary(logist.model01 )
 
-# selecionando variáveis por método automático
+# selecionando vari?veis por m?todo autom?tico
 
 model_step<-step(logist.model01,direction="both")
 
@@ -27,24 +27,26 @@ summary(Titanic_train$predito )
 
 hist(Titanic_train$predito)
 
-# Criar variável faixa probabilidade
+# Criar vari?vel faixa probabilidade
 Titanic_train$fx_predito1 <- cut(Titanic_train$predito, breaks=c(0,0.10,0.20,0.30,0.40,0.50,0.60,0.70,0.80,0.90,1), right=F)
-# Frequência absoluta
+# Frequ?ncia absoluta
 table(Titanic_train$fx_predito1,Titanic_train$Survived)
 
-# Frequência relativa
+View(Titanic_train)
+
+# Frequ?ncia relativa
 print(prop.table(table(Titanic_train$fx_predito1,Titanic_train$Survived),2), digits=2)
 
 plot(Titanic_train$fx_predito1 , Titanic_train$Survived)
 
-### Matriz de confusão - Escolhendo o corte em 0.38 
+### Matriz de confus?o - Escolhendo o corte em 0.38 
 
 Titanic_train$fx_predito <- cut(Titanic_train$predito, breaks=c(0,0.38,1), right=F)
 
-MC_log_treino <- table(Titanic_train$Survived, Titanic_train$fx_predito , deparse.level = 2) # montar a matriz de confusão  
+MC_log_treino <- table(Titanic_train$Survived, Titanic_train$fx_predito , deparse.level = 2) # montar a matriz de confus?o  
 show(MC_log_treino) # mostra os resultados  
-ACC_log = sum(diag(MC_log_treino))/sum(MC_log_treino)*100 # calcula a acurácia  
-show(ACC_log) # mostra a acurácia  
+ACC_log = sum(diag(MC_log_treino))/sum(MC_log_treino)*100 # calcula a acur?cia  
+show(ACC_log) # mostra a acur?cia  
 
 
 # e se aplicar ma amostra teste?
